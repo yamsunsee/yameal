9367957f9bf3f01338ba63a790ec03baf5a818df
Yamsunsee <yamsunsee@gmail.com>
2022-06-30 01:31:12 +0700

    Responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,53 +1,79 @@
+// Sliders
 $(document).ready(function () {
   $(".hero").slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
+    arrows: false,
   })
-})
 
-$(document).ready(function () {
   $(".categories").slick({
-    infinite: true,
+    infinite: false,
     slidesToShow: 3,
     slidesToScroll: 3,
     arrows: true,
-    prevArrow: `<div id="prev" class="w-12 h-12 rounded-lg border-2 border-gray-300 flex justify-center items-center text-gray-300 text-xl cursor-pointer transition-all hover:text-white hover:bg-orange-400 hover:border-orange-400">
-    <ion-icon name="arrow-back"></ion-icon>
-  </div>`,
-    nextArrow: `<div id="next" class="w-12 h-12 rounded-lg border-2 border-gray-300 flex justify-center items-center text-gray-300 text-xl cursor-pointer transition-all hover:text-white hover:bg-orange-400 hover:border-orange-400">
-    <ion-icon name="arrow-forward"></ion-icon>
-  </div>`,
+    prevArrow: `<div id="prev"><ion-icon name="arrow-back"></ion-icon></div>`,
+    nextArrow: `<div id="next"><ion-icon name="arrow-forward"></ion-icon></div>`,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   })
-})
 
-$(document).ready(function () {
   $(".menu").slick({
-    infinite: true,
+    infinite: false,
     slidesToShow: 2,
     slidesToScroll: 2,
     arrows: true,
-    prevArrow: `<div id="prev2" class="w-12 h-12 rounded-lg border-2 border-gray-300 flex justify-center items-center text-gray-300 text-xl cursor-pointer transition-all hover:text-white hover:bg-orange-400 hover:border-orange-400">
-    <ion-icon name="arrow-back"></ion-icon>
-  </div>`,
-    nextArrow: `<div id="next2" class="w-12 h-12 rounded-lg border-2 border-gray-300 flex justify-center items-center text-gray-300 text-xl cursor-pointer transition-all hover:text-white hover:bg-orange-400 hover:border-orange-400">
-    <ion-icon name="arrow-forward"></ion-icon>
-  </div>`,
+    prevArrow: `<div id="prev2"><ion-icon name="arrow-back"></ion-icon></div>`,
+    nextArrow: `<div id="next2"><ion-icon name="arrow-forward"></ion-icon></div>`,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   })
-})
 
-$(document).ready(function () {
   $(".testimonial").slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
-    prevArrow: `<div id="prev3" class="w-12 h-12 rounded-lg border-2 border-gray-300 flex justify-center items-center text-gray-300 text-xl cursor-pointer transition-all hover:text-white hover:bg-orange-400 hover:border-orange-400">
-    <ion-icon name="arrow-back"></ion-icon>
-  </div>`,
-    nextArrow: `<div id="next3" class="w-12 h-12 rounded-lg border-2 border-gray-300 flex justify-center items-center text-gray-300 text-xl cursor-pointer transition-all hover:text-white hover:bg-orange-400 hover:border-orange-400">
-    <ion-icon name="arrow-forward"></ion-icon>
-  </div>`,
+    dots: true,
+    arrows: false,
   })
+})
+
+// Menu Items
+const menuItems = [...document.querySelectorAll(".menu-item")]
+const firstItem = menuItems[0]
+let currentSelectedItem = firstItem
+
+menuItems.forEach((menuItem, index) => {
+  menuItem.onclick = ({ target }) => {
+    if (target !== currentSelectedItem) {
+      target.classList.remove("hoverable")
+      target.classList.add("selected")
+      firstItem.style.setProperty("--value", `${index * 76}px`)
+      currentSelectedItem.classList.remove("selected")
+      currentSelectedItem.classList.add("hoverable")
+      currentSelectedItem = target
+    }
+  }
 })
